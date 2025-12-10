ae22a20c863a43ca8c9a8442790bf8712343b90c
Dixita <contact@dixita.dev>
2025-12-10 21:03:38 +0530

    build: migrate to jaspr version0.22.0
    - ref: https://docs.jaspr.site/releases/v/0.22.0

---
+++ lib/main.server.options.dart
@@ -0,0 +1,40 @@
+// dart format off
+// ignore_for_file: type=lint
+
+// GENERATED FILE, DO NOT MODIFY
+// Generated with jaspr_builder
+
+import 'package:jaspr/server.dart';
+import 'package:jaspr_content/components/theme_toggle.dart' as _theme_toggle;
+import 'package:jaspr_site/components/footer.dart' as _footer;
+import 'package:jaspr_site/components/header.dart' as _header;
+
+/// Default [ServerOptions] for use with your Jaspr project.
+///
+/// Use this to initialize Jaspr **before** calling [runApp].
+///
+/// Example:
+/// ```dart
+/// import 'main.server.options.dart';
+///
+/// void main() {
+///   Jaspr.initializeApp(
+///     options: defaultServerOptions,
+///   );
+///
+///   runApp(...);
+/// }
+/// ```
+ServerOptions get defaultServerOptions => ServerOptions(
+  clientId: 'main.client.dart.js',
+  clients: {
+    _theme_toggle.ThemeToggle: ClientTarget<_theme_toggle.ThemeToggle>(
+      'jaspr_content:theme_toggle',
+    ),
+  },
+  styles: () => [
+    ..._theme_toggle.ThemeToggleState.styles,
+    ..._footer.Footer.styles,
+    ..._header.Header.styles,
+  ],
+);
